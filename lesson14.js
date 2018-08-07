a35e4e8422dd397c7c493b468c7beadc945676a4
Peter Horton <phorton@softstackfactory.org>
2018-08-07 17:04:48 +0000

    changed data type to array

--- lesson14.js
+++ lesson14.js
@@ -7,20 +7,20 @@ var people = [
     { name: "John", age: 55 },
     { name: "Roger", age: 44 },
     { name: "Betty", age: 37 }
-    ]
+    ];
 
 
 var nameFilter = function(filteredName) {
-    var results = {}
+    var results = [];
     for (var i = 0; i < people.length; i++) {
-        let currentName = people[i].name
-        if (currentName != filteredName) {
-            results.push(currentName)
+        let currentName = people[i].name;
+        if (currentName !== filteredName) {
+            results.push(currentName);
         }
     }
-    return results
+    return results;
 }
 
 
-var filteredNames = nameFilter()
-console.log(filteredNames)
+var filteredNames = nameFilter("Betty");
+console.log(filteredNames);
